d499ba8cb116a848bd86b03db7dc34b46c283613
Daro <twojemail@example.com>
2025-05-30 22:47:04 +0200

    Creating Clock component for time opperation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
 
+  return (
     <>
 
     </>
+  )
+}
 
-export default App;
+export default App
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,8 +1,17 @@
+import {useEffect, useState} from "react";
+
 const Clock = () => {
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
 
-    return();
+        return () => clearInterval(interval);
+    }, []);
 
-}
+    return <h2>Aktualny czas: {time}</h2>;
+};
 
 export default Clock;
