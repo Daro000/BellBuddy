975f65e90215e6ad7f7d7174526ee3ac6f6ed518
Daro <twojemail@example.com>
2025-05-30 23:10:33 +0200

    Creating component for students present

--- src/StudentItem.tsx
+++ src/StudentItem.tsx
@@ -1,4 +1,19 @@
-const studentItem = () => {
-    return();
-}
-export default studentItem;
+import React from "react";
+
+type Props = {
+    id: number;
+    name: string;
+    present: boolean;
+    togglePresence: (id: number) => void;
+};
+
+const StudentItem = ({ id, name, present, togglePresence }: Props) => {
+    return (
+        <li>
+            {name} - {present ? "Obecny" : "Nieobecny"}
+            <button onClick={() => togglePresence(id)}>Zmień status</button>
+        </li>
+    );
+};
+
+export default StudentItem;
